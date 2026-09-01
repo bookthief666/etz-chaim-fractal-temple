b4e73831d56c70f3ffad1c6fa46e56c381eb864b
bookthief666 <manuel.orrantia@gmail.com>
2026-09-01 03:51:43 -0700

    Preserve M4.15 lineage in M4.15.1 build identity

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -1,4 +1,8 @@
 export const BUILD_INFO = Object.freeze({
+  baseline: Object.freeze({
+    milestone: 'M4.15',
+    runtime: 'm4.15.0',
+  }),
   milestone: 'M4.15.1',
   codename: 'Realm Ignition Guard',
   runtime: 'm4.15.1',
