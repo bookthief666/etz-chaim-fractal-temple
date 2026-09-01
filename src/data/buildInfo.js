3c4f18b94cdb77a1ec7b87a33b5bbd81dab4df50
bookthief666 <manuel.orrantia@gmail.com>
2026-09-01 03:44:57 -0700

    M4.15.1 stamp realm ignition guard runtime

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -1,5 +1,5 @@
 export const BUILD_INFO = Object.freeze({
-  milestone: 'M4.15',
-  codename: 'Renderer Ownership / Realm Isolation',
-  runtime: 'm4.15.0',
+  milestone: 'M4.15.1',
+  codename: 'Realm Ignition Guard',
+  runtime: 'm4.15.1',
 })
